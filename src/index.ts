4f19355da17129c8e770eae1e32176a570ee7977
Binod Nepali <nepalibinod9@gmail.com>
2023-07-16 21:48:46 +0200

    chore: export date picker

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 import './index.css';
 export { NepaliCalendar } from './components/NepaliCalendar/NepaliCalendar';
+export { NepaliDatePicker } from './components/NepaliDatePicker/NepaliDatePicker';
