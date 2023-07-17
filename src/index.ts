714d33e783b2e926065c36b91a6204576f68a645
Binod Nepali <nepalibinod9@gmail.com>
2023-07-17 16:50:38 +0200

    chore: update exports from root index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 import './index.css';
-export { NepaliCalendar } from './components/NepaliCalendar/NepaliCalendar';
-export { NepaliDatePicker } from './components/NepaliDatePicker/NepaliDatePicker';
+export * from './components';
