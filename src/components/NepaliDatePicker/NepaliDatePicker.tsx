e686860521aa073382c990fc16dc9fcbfe43c3e9
Binod Nepali <nepalibinod9@gmail.com>
2023-07-18 15:10:33 +0200

    fix(datepicker): margin top in mobile

--- src/components/NepaliDatePicker/NepaliDatePicker.tsx
+++ src/components/NepaliDatePicker/NepaliDatePicker.tsx
@@ -64,7 +64,7 @@ export const NepaliDatePicker = ({
       {showModal && (
         <Modal
           onClose={() => setShowModal(() => false)}
-          className='mt-11 rounded-md'
+          className='md:mt-11 rounded-md'
         >
           <NepaliCalendar
             onDateSelect={handleOnSelectDate}
