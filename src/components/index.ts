6e06f38f538b81a6cad10a9db165267151f1f96a
Binod Nepali <nepalibinod9@gmail.com>
2023-08-01 19:28:45 +0200

    chore: rename export name for calander and datepicker

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,14 @@
 import { DateInput } from '@/components/DateInput/DateInput'
 import { DesktopTime, DesktopTimePicker } from '@/components/DesktopTimePicker'
-import { NepaliCalendar } from '@/components/NepaliCalendar/NepaliCalendar'
-import { NepaliDatePicker } from '@/components/NepaliDatePicker/NepaliDatePicker'
+import { NepaliCalendar as Calendar } from '@/components/NepaliCalendar/NepaliCalendar'
+import { NepaliDatePicker as DatePicker } from '@/components/NepaliDatePicker/NepaliDatePicker'
 import { TimeInput } from '@/components/TimeInput/TimeInput'
 
 export {
+  Calendar,
   DateInput,
+  DatePicker,
   DesktopTime,
   DesktopTimePicker,
-  NepaliCalendar,
-  NepaliDatePicker,
   TimeInput,
 }
