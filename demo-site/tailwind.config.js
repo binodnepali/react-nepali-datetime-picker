6f75c3232ceb23bd8a83058b35d31e7eb790e1cf
Binod Nepali <nepalibinod9@gmail.com>
2023-12-29 13:21:52 +0100

    chore: add demo site

---
+++ demo-site/tailwind.config.js
@@ -0,0 +1,10 @@
+import { nedtTwPlugin } from 'react-nepali-datetime-picker'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [nedtTwPlugin()],
+}
